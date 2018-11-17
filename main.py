7869102b626b807ad1b8bac2c1a0594e9ba7f6fd
sweetsbeats <the.patton@yahoo.ca>
2018-11-16 19:49:33 -0800

    Determines if images are the same or not

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
+
 import cv2
+import numpy
 
-img = cv2.imread("totoro.jpg")
-print img.size
-print img.dtype
+img1 = cv2.imread("totoro.jpg")
+img2 = cv2.imread("totworo.jpg");
 
-cv2.namedWindow('Totoro', cv2.WINDOW_NORMAL)
-cv2.resizeWindow("Totoro", 1280, 720) 
+print "debug info:"
+print "Image 1"
+print img1.size
+print img1.dtype
 
-cv2.imshow("Totoro", img)
+print "\n"
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+print "Image 2"
+print img2.size
+print img2.dtype
+
+diff = cv2.subtract(img1, img2)
+
+if numpy.any(diff):
+    cv2.namedWindow('Differences', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("Differences", 1280, 720) 
+
+    cv2.imshow("Differences", diff)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+else:
+    print "Images are the same"
